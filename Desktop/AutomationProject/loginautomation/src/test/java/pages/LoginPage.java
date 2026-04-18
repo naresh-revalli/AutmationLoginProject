77e54daa5030471318c5fa6cfee21db8e51e6352
naresh <nareshrevalli57@gmail.com>
2026-04-18 14:38:12 +0530

    first commit

---
+++ Desktop/AutomationProject/loginautomation/src/test/java/pages/LoginPage.java
@@ -0,0 +1,31 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Locators
+    By email = By.xpath("//input[@type='email']");
+    By password = By.xpath("//input[@type='password']");
+    By loginBtn = By.xpath("//button[text()='Sign In']");
+
+    // Actions
+    public void enterEmail(String mail) {
+        driver.findElement(email).sendKeys(mail);
+    }
+
+    public void enterPassword(String pass) {
+        driver.findElement(password).sendKeys(pass);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginBtn).click();
+    }
+}
